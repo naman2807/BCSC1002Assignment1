327a06b4babd97c7f20cc00916dcf7fa477d4fa2
naman2807 <naman.agarwal_cs19@gla.ac.in>
2020-08-27 11:56:59 +0530

    override equals() and hashCode() method for Student.java class

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -7,6 +7,7 @@
 package definitions;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Student {
     private String[] nameOfStudent;
@@ -79,5 +80,24 @@ public class Student {
                 "\nUniversity Roll Number: " + universityRollNumber +
                 "\nNumber Of Books Issued: " + numberOfBooksIssued +
                 "\nIssued Books: " + Arrays.toString(issuedBooks);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return universityRollNumber == student.universityRollNumber &&
+                numberOfBooksIssued == student.numberOfBooksIssued &&
+                Arrays.equals(nameOfStudent, student.nameOfStudent) &&
+                Arrays.equals(issuedBooks, student.issuedBooks);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(universityRollNumber, numberOfBooksIssued);
+        result = 31 * result + Arrays.hashCode(nameOfStudent);
+        result = 31 * result + Arrays.hashCode(issuedBooks);
+        return result;
     }
 }
