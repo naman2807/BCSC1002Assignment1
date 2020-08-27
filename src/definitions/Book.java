ecb7f3b9dafb2117e087443174317bfa3a442a44
naman2807 <naman.agarwal_cs19@gla.ac.in>
2020-08-27 11:01:33 +0530

    create getter and setter method for Book class

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -16,4 +16,28 @@ public class Book {
         this.authorName = authorName;
         this.bookISBNNumber = bookISBNNumber;
     }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getBookISBNNumber() {
+        return bookISBNNumber;
+    }
+
+    public void setBookISBNNumber(String bookISBNNumber) {
+        this.bookISBNNumber = bookISBNNumber;
+    }
 }
