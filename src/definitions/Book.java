1deb4423ebe628b39c41615011024d6359a1ea50
naman2807 <naman.agarwal_cs19@gla.ac.in>
2020-08-27 11:00:50 +0530

    create constructor for Book class

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -11,5 +11,9 @@ public class Book {
     private String authorName;
     private String bookISBNNumber;
 
-
+    public Book(String bookName, String authorName, String bookISBNNumber) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.bookISBNNumber = bookISBNNumber;
+    }
 }
