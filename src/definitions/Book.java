52f903a7b86a74de8c33273de719a8e1a0c926b7
naman2807 <naman.agarwal_cs19@gla.ac.in>
2020-08-27 11:02:56 +0530

    override equals() and hashCode() method for Book class

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -6,6 +6,8 @@
  * */
 package definitions;
 
+import java.util.Objects;
+
 public class Book {
     private String bookName;
     private String authorName;
@@ -39,5 +41,20 @@ public class Book {
 
     public void setBookISBNNumber(String bookISBNNumber) {
         this.bookISBNNumber = bookISBNNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(bookName, book.bookName) &&
+                Objects.equals(authorName, book.authorName) &&
+                Objects.equals(bookISBNNumber, book.bookISBNNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, authorName, bookISBNNumber);
     }
 }
