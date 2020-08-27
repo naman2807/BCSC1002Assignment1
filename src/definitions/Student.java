28cf50597a881335def302dda587ab7cf70cc745
naman2807 <naman.agarwal_cs19@gla.ac.in>
2020-08-27 11:39:34 +0530

    create member variables for Student.java class

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -7,4 +7,9 @@
 package definitions;
 
 public class Student {
+    private String[] nameOfStudent;
+    private long universityRollNumber;
+    private int numberOfBooksIssued;
+    private Book[] issuedBooks;
+
 }
